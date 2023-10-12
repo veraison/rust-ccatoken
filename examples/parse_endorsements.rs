1b736f5569bad171b5f3577cf96c26fbf56355cd
Thomas Fossati <thomas.fossati@linaro.org>
2023-10-12 11:20:30 +0200

    initial layout
    Signed-off-by: Thomas Fossati <thomas.fossati@linaro.org>

---
+++ examples/parse_endorsements.rs
@@ -0,0 +1,10 @@
+// Copyright 2023 Contributors to the Veraison project.
+// SPDX-License-Identifier: Apache-2.0
+
+extern crate ccatoken;
+
+use ccatoken::*;
+
+fn main() {
+    parse_endorsements();
+}
