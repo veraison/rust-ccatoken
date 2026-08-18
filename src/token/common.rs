3234c8e9380921d9025a6c685bbb18dda7153111
Qirui Huang <qirui.huang@arm.com>
2026-08-18 17:17:51 +0100

    feat: add SHA-384 as a valid identifier for Platform hash-algo-id, Realm hash-algo-id, and Realm public-key-hash-algo-id.
    Signed-off-by: Qirui Huang <qirui.huang@arm.com>

--- src/token/common.rs
+++ src/token/common.rs
@@ -6,9 +6,9 @@ use ciborium::Value;
 
 // Among the admissible values in
 // https://www.iana.org/assignments/hash-function-text-names/hash-function-text-names.xhtml
-// we only support sha-256 and sha-512 as per RMM specification
+// we only support sha-256, sha-384 and sha-512 as per RMM specification
 pub fn is_valid_hash(value: &str) -> bool {
-    matches!(value, "sha-256" | "sha-512")
+    matches!(value, "sha-256" | "sha-384" | "sha-512")
 }
 
 pub fn is_valid_measurement(value: &[u8]) -> bool {
